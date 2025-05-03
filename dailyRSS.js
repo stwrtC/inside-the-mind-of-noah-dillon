702107f23075d30fcf0d59f5866f1ffe3bae6ad4
Connor <c.stewart12@uni.brighto.acc.uk>
2025-05-03 22:42:52 +0100

    intitial commit

---
+++ dailyRSS.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const RSS = require('rss');
+
+const data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+const historyPath = './history.json';
+const history = fs.existsSync(historyPath)
+  ? JSON.parse(fs.readFileSync(historyPath, 'utf8'))
+  : [];
+
+// Filter unused posts
+const unusedPosts = data.filter(post => !history.includes(post.id));
+
+if (unusedPosts.length === 0) {
+  console.log('All posts have been used!');
+  process.exit(0);
+}
+
+// Pick a random unused post
+const item = unusedPosts[Math.floor(Math.random() * unusedPosts.length)];
+
+// Create RSS feed
+const feed = new RSS({
+  title: 'Inside the Mind of Noah Dillon',
+  description: '753 tweets spanning from 2014 to 3rd May 2025',
+  feed_url: 'http://example.com/feed.xml',
+  site_url: 'http://example.com',
+  pubDate: new Date()
+});
+
+feed.item({
+  title: item.owner_display_name,
+  description: item.tweet_text,
+  creation: item.created_at,
+  replies: item.reply_count,
+  retweets: item.retweet_count,
+  favourites: item.favourite_count,
+  url: item.address,
+  date: new Date()
+});
+
+
+fs.writeFileSync('./feed.xml', feed.xml({ indent: true }));
+console.log(`Posted: ${item.title}`);
+
+// Update history
+history.push(item.id);
+fs.writeFileSync(historyPath, JSON.stringify(history, null, 2));
